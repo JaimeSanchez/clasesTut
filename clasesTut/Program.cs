1fa925c6b7454746063a795517769edc1943a36e
famsanpra <emiajh@gmail.com>
2016-08-13 14:42:08 -0500

    Agregar archivos de proyecto.

---
+++ clasesTut/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasesTut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declaracion del objeto empleado
+            clsEmpleados empleado;
+            //Instanciacion de la variable empleado;
+            empleado = new clsEmpleados();
+
+            empleado.edad = 25;
+            empleado.Nombre = "Jaime Sánchez";
+            empleado.SueldoDiario = 500m;
+
+            decimal total;
+
+            total = empleado.CalculaSalario(30);
+            Console.WriteLine("El salario mensual del empleado "+ empleado.Nombre);
+            Console.WriteLine("es: " + total.ToString("C"));
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ clasesTut/clsEmpleados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clasesTut
+{
+    public class clsEmpleados
+    {
+        //metodo constructor o constructor
+        public clsEmpleados()
+        {
+            Nombre = "";
+            SueldoDiario = 0.0M;
+            edad = 0;
+        }
+
+        //propiedades de la clase de tipo publico para poder accesar a ellas
+        public string Nombre;
+        public decimal SueldoDiario;
+        public int edad;
+
+        //metodos de la clase
+        //metodo para calcular los dias de salario
+        public decimal CalculaSalario(int NumeroDias)
+        {
+            return SueldoDiario * NumeroDias;
+
+        }
+
+    }
+}
